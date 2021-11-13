dc37a4ff32d25d272f4086f620ca054458829e24
Robin Boers <robindev2019@outlook.com>
2021-11-13 16:45:43 +0100

    Made alerts float

--- lib/nindo_phx_web/views/components/alert.ex
+++ lib/nindo_phx_web/views/components/alert.ex
@@ -17,7 +17,7 @@ defmodule NindoPhxWeb.AlertComponent do
   # bg-green-500 bg-green-500 bg-red-500
   def alert(assigns) do
     ~H"""
-      <div class="text-center py-4 lg:px-4">
+      <div class="absolute text-center w-full lg:px-4">
         <div class={"p-2 bg-#{@color}-800 items-center text-#{@color}-100 leading-none lg:rounded-full flex lg:inline-flex"} role="alert">
           <span class={"flex rounded-full bg-#{@color}-500 uppercase px-2 py-1 text-xs font-bold mr-3"}><%= @title %></span>
           <span class="font-semibold mr-2 text-left flex-auto"><%= @message %></span>
