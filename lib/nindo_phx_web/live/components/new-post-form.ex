eabe0465b99bda716f05564932cd7c99af2021a0
Robin Boers <robindev2019@outlook.com>
2021-12-18 19:40:41 +0100

    Black magic

--- lib/nindo_phx_web/live/components/new-post-form.ex
+++ lib/nindo_phx_web/live/components/new-post-form.ex
@@ -9,10 +9,10 @@ defmodule NindoPhxWeb.Live.Components.NewPost do
   def render(assigns) do
     ~H"""
     <section class="new-post-section">
-      <button class="new-post-btn btn-primary mb-6" onclick="newPost()" phx-update="ignore">New post</button>
-      <div class="new-post-modal transition-height h-0 overflow-y-hidden w-full" phx-update="ignore">
+      <button class="new-post-btn btn-primary mb-6" onclick="newPost()">New post</button>
+      <div class="new-post-modal transition-height h-0 overflow-y-hidden w-full">
           <div class="mb-6 p-1">
-            <.form let={f} for={@changeset} class="new-post-form w-full" phx-change="validate" phx-submit="publish" phx-target={@myself}>
+            <.form let={f} for={@changeset} class="new-post-form w-full" phx-submit="publish" phx-target={@myself}>
               <%= text_input f, :title, placeholder: "Title", class: "new-post-form-title w-full mb-2 input block resize-none flex-grow" %>
               <%= textarea f, :body, autofocus: "autofocus", placeholder: "Write something inspirational... ", class: "new-post-form-body w-full input block resize-none flex-grow" %>
               <button class="new-post-submit btn-primary mt-2" type="submit">Publish</button>
@@ -42,26 +42,18 @@ defmodule NindoPhxWeb.Live.Components.NewPost do
   end
 
   @impl true
-  def handle_event("validate", %{"post" => %{"body" => body, "title" => title}}, socket)
-  when socket.assigns.user != nil do
-    changeset = Posts.validate(title, body, nil, socket.assigns.user)
-
-    {:noreply, socket
-    |> assign(:changeset, changeset)
-    |> assign(:error, format_error(changeset))}
-  end
-
-  @impl true
   def handle_event("publish", %{"post" => post}, socket)
   when socket.assigns.user != nil do
     user = socket.assigns.user
-    IO.inspect("Publishing new post.")
 
     case Posts.new(post["title"] , post["body"], nil, user) do
       {:ok, _post}    ->
         Task.async(fn -> Feeds.update_cache(user) end)
         send(self(), :refresh)
-        {:noreply, socket}
+
+        changeset = Posts.validate(nil, nil, nil, %{id: 0})
+        {:noreply, socket
+        |> assign(:changeset, changeset)}
       {:error, error} ->
         {:noreply, socket
         |> assign(:error, format_error(error))}
