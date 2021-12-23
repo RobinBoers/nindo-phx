699843abbf6ee407558f9714007a3531b295b460
Robin Boers <robindev2019@outlook.com>
2021-12-23 16:43:28 +0100

    Fixed wrong domain in prod.exs

--- config/prod.exs
+++ config/prod.exs
@@ -1,7 +1,7 @@
 import Config
 
 config :nindo_phx, NindoPhxWeb.Endpoint,
-  url: [host: "nindo.fly.dev", port: 80],
+  url: [host: "nindo.geheimesite.nl", port: 80],
   cache_static_manifest: "priv/static/cache_manifest.json"
 
 config :logger, level: :warn
